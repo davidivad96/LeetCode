c1108a2da10153d45273061246e7adc33f6257be
davidivad96 <davidivad96@gmail.com>
2021-05-03 20:24:32 +0200

    Solved running sum of 1d array problem.

--- easy/running_sum_of_1d_array/running_sum_of_1d_array.rs
+++ easy/running_sum_of_1d_array/running_sum_of_1d_array.rs
@@ -1,3 +1,14 @@
+fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+    let mut result: Vec<i32> = vec![];
+    let mut sum: i32 = 0;
+    for num in nums {
+        sum += num;
+        result.push(sum);
+    }
+    result
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums: Vec<i32> = vec![3, 1, 2, 10, 1];
+    println!("Running sum: {:?}", running_sum(nums));
 }
