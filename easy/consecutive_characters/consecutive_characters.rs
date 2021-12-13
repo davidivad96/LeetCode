bf161f930ea30f4db3e24fa6d2035f534664d818
davidivad96 <david@strivelabs.io>
2021-12-13 13:39:25 +0100

    Solve "Consecutive Characters" problem

--- easy/consecutive_characters/consecutive_characters.rs
+++ easy/consecutive_characters/consecutive_characters.rs
@@ -1,8 +1,24 @@
 fn max_power(s: String) -> i32 {
-    2
+    let mut max: i32 = 1;
+    let mut sum: i32 = 1;
+    let mut i: usize = 0;
+    while i < s.len() - 1 {
+        let current: char = s.chars().nth(i).unwrap();
+        let next: char = s.chars().nth(i + 1).unwrap();
+        if current == next {
+            sum += 1;
+            if sum > max {
+                max = sum
+            }
+        } else {
+            sum = 1
+        }
+        i += 1;
+    }
+    max
 }
 
 fn main() {
-    let s: String = "leetcode".to_string();
+    let s: String = "hooraaaaaaaaaaay".to_string();
     println!("Power of s: {}", max_power(s));
 }
