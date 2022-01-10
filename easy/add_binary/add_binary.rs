f2e0c58770bf863ba7bfff3fbb18139cbe3d7ad7
davidivad96 <david@strivelabs.io>
2022-01-10 10:44:43 +0100

    Solve Add Binary problem

--- easy/add_binary/add_binary.rs
+++ easy/add_binary/add_binary.rs
@@ -1,9 +1,11 @@
 fn add_binary(a: String, b: String) -> String {
-    "david".to_string()
+    let a_decimal = i128::from_str_radix(&a, 2).unwrap();
+    let b_decimal = i128::from_str_radix(&b, 2).unwrap();
+    format!("{:b}", a_decimal + b_decimal)
 }
 
 fn main() {
-    let a: String = "11".to_string();
-    let b: String = "1".to_string();
+    let a: String = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101".to_string();
+    let b: String = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011".to_string();
     println!("Sum of binary strings: {}", add_binary(a, b));
 }
