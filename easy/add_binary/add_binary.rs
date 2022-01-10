0054401e2895b8cf078050fc29cc59b8b7b826d2
davidivad96 <david@strivelabs.io>
2022-01-10 10:35:39 +0100

    Init Add Binary problem

---
+++ easy/add_binary/add_binary.rs
@@ -0,0 +1,9 @@
+fn add_binary(a: String, b: String) -> String {
+    "david".to_string()
+}
+
+fn main() {
+    let a: String = "11".to_string();
+    let b: String = "1".to_string();
+    println!("Sum of binary strings: {}", add_binary(a, b));
+}
