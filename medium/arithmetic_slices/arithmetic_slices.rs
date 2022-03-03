6433bb5a8f609068481c5abff98d534498a5d6a6
davidivad96 <david@strivelabs.io>
2022-03-03 21:44:13 +0100

    Refactor Arithmetic Slices solution

--- medium/arithmetic_slices/arithmetic_slices.rs
+++ medium/arithmetic_slices/arithmetic_slices.rs
@@ -5,8 +5,8 @@ fn number_of_arithmetic_slices(nums: Vec<i32>) -> i32 {
     let mut sum = 0;
     for i in 0..nums.len() - 2 {
         let diff = nums[i] - nums[i + 1];
-        let mut j = 0;
-        while i + j + 2 < nums.len() && nums[i + j + 1] - nums[i + j + 2] == diff {
+        let mut j = i;
+        while j + 2 < nums.len() && nums[j + 1] - nums[j + 2] == diff {
             sum += 1;
             j += 1;
         }
